d1966751b1e396466967c20b969cd836fecfba25
ice cream david <acompletelyrandomrat@gmail.com>
2025-04-04 10:11:55 -0600

    Update IDrawable.cs

--- IDrawable.cs
+++ IDrawable.cs
@@ -1,5 +1,6 @@
 ﻿namespace Monogon.Engine;
 
+// Will likely be removed entirly as game logic and engine logic are similar enough to where this isn´t justified.
 public interface IDrawable
 {
     public void Draw(float deltaTime, MonogonEngine monogon);
