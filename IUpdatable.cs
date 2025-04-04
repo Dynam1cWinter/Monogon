e2c5086f175008fb3ec2b144b51bec1dc93a3e33
ice cream david <acompletelyrandomrat@gmail.com>
2025-04-04 10:12:58 -0600

    Update IUpdatable.cs

--- IUpdatable.cs
+++ IUpdatable.cs
@@ -2,6 +2,7 @@
 
 namespace Monogon.Engine;
 
+// Will likely be removed entirly as game logic and engine logic are similar enough to where this isn´t justified.
 public interface IUpdatable
 {
     public void Update(float deltaTime, MonogonEngine monogon);
